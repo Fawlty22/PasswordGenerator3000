f95e468c571941c4451e55873112f9d5971b7c47
Fawlty22 <85969343+Fawlty22@users.noreply.github.com>
2021-09-28 15:19:05 -0600

    Add files via upload
    I do not understand why, but git is not letting me push only my passwordgenerator files to github.  It says i have an embedded repository and therefore cannot push.  Thus, I am manually uploaded these files because I am sick of it.

---
+++ assets/js/script2.js
@@ -0,0 +1,94 @@
+/*Define strings for characters*/
+let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let lowercase = 'abcdefghijklmnopqrstuvwxyz'
+let numbers = '0123456789'
+let specialcase = '!@#$%^&*'
+let newPasswordPre = ''
+let newPassword = ''
+/*Scope-problem Variables*/
+let trueUppercase = false
+let trueLowercase = false
+let trueNumbers = false
+let trueSpecialcase = false
+let passLength = ''
+
+/*Generate Length Function*/
+function generateLength() {
+    passLength = prompt('How many characters should your password include?')
+    
+    if (passLength != Number) {
+        passLength = parseInt(passLength, 10)
+    }
+
+    while (passLength < 8 || passLength > 128) {
+    confirm('Please choose a length between 8 and 128 characters.')
+    passLength = prompt('How many characters should your password include?')
+    }
+}
+
+/*Generate Character Types Function*/
+function generateCharacterTypes() {
+    
+    trueUppercase = confirm('Would you like to include uppercase characters?')
+    trueLowercase = confirm('Would you like to include lowercase characters?')
+    trueNumbers = confirm('Would you like to include numbers?')
+    trueSpecialcase = confirm('Would you like to include special characters?')
+}
+
+/*Generate Password Function*/
+function generatePassword() {
+    generateLength();
+    generateCharacterTypes();
+
+if (trueUppercase || trueLowercase || trueNumbers || trueSpecialcase) {
+    for (i = 0; i < passLength; i++){
+        if (trueUppercase) {
+            var randomNumber = Math.floor(Math.random() * uppercase.length);
+            newPasswordPre += uppercase[randomNumber];
+        }
+        if (trueLowercase) {
+            var randomNumber = Math.floor(Math.random() * lowercase.length);
+            newPasswordPre += lowercase[randomNumber];
+        }
+        if (trueNumbers) {
+            var randomNumber = Math.floor(Math.random() * numbers.length);
+            newPasswordPre += numbers[randomNumber];
+        }
+        if (trueSpecialcase) {
+            var randomNumber = Math.floor(Math.random() * specialcase.length);
+            newPasswordPre += specialcase[randomNumber];
+        }
+    }
+
+    for (i = 0; i < passLength; i++){
+        var randomNumber = Math.floor(Math.random() * newPasswordPre.length);
+        newPassword += newPasswordPre[randomNumber];
+    }   
+}
+/* Nothing Chosen */
+else {
+    alert('Please choose at least ONE character type.')
+    generatePassword();
+}
+
+return newPassword;
+}
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+
+  }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
